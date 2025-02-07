1990f1fad3331c64c0e5d0f1d3451afac1bf0f9e
John Clem <johnnyclem@gmail.com>
2025-02-07 09:59:36 -0600

    simplify to the max

--- components/container.js
+++
@@ -1,9 +0,0 @@
-import { cx } from "@utils/all";
-
-export default function Container(props) {
-  return (
-    <div className={cx("container mx-auto px-5", props.className)}>
-      {props.children}
-    </div>
-  );
-}
